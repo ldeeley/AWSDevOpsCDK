0c1d42a5e1e94c9acf5325f56b8d4ea2297bc718
ldeeley <lester.deeley@yahoo.com>
2023-05-09 15:14:45 +0100

    Initial

---
+++ src/main/java/com/example/awsdevopscdk/AwsDevOpsCdkApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsdevopscdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevOpsCdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDevOpsCdkApplication.class, args);
+    }
+
+}
